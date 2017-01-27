710d96b1baa1d3211624c6bef95f391def29f600
alyssavelia <alyssavelia2980@gmail.com>
2017-01-27 21:29:56 +0700

    commit 1

---
+++ src/Pencarian_Sederhana_naik.java
@@ -0,0 +1,20 @@
+import java.*;
+import javax.swing.*;
+
+public class Pencarian_Sederhana_naik{
+    public static void main (String[] args){
+        try{
+        int [] DataArray={0,1,2,3,4,5,6,7,8,9,10 };
+        int i, NilaiX;
+        boolean ketemu;
+        
+        System.out.println("");
+        System.out.println("Program pencarian data sederhana");
+        System.out.println("=================================");
+        for(i=0;i<11;i++);
+        
+        
+        }
+    }
+    
+}
